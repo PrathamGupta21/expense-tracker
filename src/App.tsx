312b4afe27718889ef4ccc22765f12d1fceaeab4
PRATHAM GUPTA <PRATHAM17GUPTA@GMAIL.COM>
2023-09-12 02:51:17 +0530

    feat: added expense filter component

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,34 @@
 import { useState } from 'react';
 import ExpenseList from './ExpenseTracker/ExpenseList';
+import ExpenseFilter from './ExpenseTracker/ExpenseFilter';
 
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState('');
   const [expenses, setExpenses] = useState([
     { id: 1, description: 'Mutton', amount: 1000, category: 'Food' },
     { id: 2, description: 'Chicken', amount: 500, category: 'Food' },
     { id: 3, description: 'Kebab', amount: 300, category: 'Food' },
-    { id: 4, description: 'Roti', amount: 200, category: 'Food' },
+    { id: 4, description: 'Maggi', amount: 200, category: 'Groceries' },
   ]);
 
+  const visibleExpenses = selectedCategory
+    ? expenses.filter((expense) => expense.category === selectedCategory)
+    : expenses;
+
   return (
-    <ExpenseList
-      expenses={expenses}
-      onDelete={(id) =>
-        setExpenses(expenses.filter((expense) => expense.id !== id))
-      }
-    />
+    <>
+      <div className='mb-3'>
+        <ExpenseFilter
+          onSelectCategory={(category) => setSelectedCategory(category)}
+        />
+      </div>
+      <ExpenseList
+        expenses={visibleExpenses}
+        onDelete={(id) =>
+          setExpenses(expenses.filter((expense) => expense.id !== id))
+        }
+      />
+    </>
   );
 }
 
